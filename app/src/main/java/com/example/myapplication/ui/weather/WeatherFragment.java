692a844e382743b481cce9dd333aecd04209f18d
ethramos22 <ethramos22@gmail.com>
2022-03-24 14:04:45 -0600

    ViewModel/Repository design pattern for WeatherFragment
    Created Weather Repository file and Edited ViewModel and Fragment to reflect architecture from lecture.

--- app/src/main/java/com/example/myapplication/ui/weather/WeatherFragment.java
+++ app/src/main/java/com/example/myapplication/ui/weather/WeatherFragment.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
 import android.os.Handler;
 import android.os.Looper;
@@ -32,6 +33,7 @@ import org.json.JSONException;
 import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.net.URL;
+import java.util.Observable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -46,9 +48,7 @@ public class WeatherFragment extends Fragment {
     private TextView mTvMaxTemp;
     private TextView mTvMinTemp;
     private ImageView mIvIcon;
-    private WeatherData mWeatherData;
     private String city;
-    private static FetchWeatherTask mFetchWeatherTask = new FetchWeatherTask();
 
     public static WeatherFragment newInstance() {
         return new WeatherFragment();
@@ -69,128 +69,53 @@ public class WeatherFragment extends Fragment {
         mTvMinTemp = (TextView) view.findViewById(R.id.tv_min_temp);
         mIvIcon = (ImageView) view.findViewById(R.id.iv_icon);
 
+        // Create View Model
+        mViewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
+
+        // Set Observer
+        mViewModel.getData().observe(getViewLifecycleOwner(), observer);
+
         // Get the data passed from the Main Activity
         try {
             city = getArguments().getString("CITY_DATA");
             mTvLocation.setText(city);
-        } catch (Exception e) {
+            loadWeatherData(city);
+        }
+        catch (Exception e) {
             String error = e.getMessage();
         }
-
-        // Check if there is any saved data from the fragment's current life cycle
-        if(savedInstanceState!=null) {
-            String temp = savedInstanceState.getString("tvTemp");
-            String hum = savedInstanceState.getString("tvHum");
-            String press = savedInstanceState.getString("tvPress");
-            String tmax = savedInstanceState.getString("tvMaxTemp");
-            String tmin = savedInstanceState.getString("tvMinTemp");
-            Bitmap icon = savedInstanceState.getParcelable("ivIcon");
-            if (temp != null)
-                mTvTemp.setText(""+temp);
-            if (hum != null)
-                mTvHum.setText(""+hum);
-            if (press != null)
-                mTvPress.setText(""+press);
-            if (tmax != null)
-                mTvPress.setText(""+tmax);
-            if (tmin != null)
-                mTvPress.setText(""+tmin);
-            if (icon != null)
-                mIvIcon.setImageBitmap(icon);
-        }
-        // If there is no weather data saved, fetch it
-        else
-        {
-            String inputFromEt = mTvLocation.getText().toString().replace(' ','&');
-            loadWeatherData(inputFromEt);
-        }
-        mFetchWeatherTask.setWeakReference(this); //make sure we're always pointing to current version of fragment
-
         return view;
     }
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("tvTemp",mTvTemp.getText().toString());
-        outState.putString("tvHum",mTvHum.getText().toString());
-        outState.putString("tvPress",mTvPress.getText().toString());
-        outState.putString("tvMaxTemp",mTvMaxTemp.getText().toString());
-        outState.putString("tvMinTemp",mTvMinTemp.getText().toString());
-        outState.putParcelable("ivIcon", mWeatherData.getCurrentCondition().getIcon());
-    }
+    final Observer<WeatherData> observer = new Observer<WeatherData>() {
+        @Override
+        public void onChanged(@Nullable final WeatherData weatherData) {
+            // Update UI
+            if (weatherData != null) {
+                mTvTemp.setText("" + Math.round(weatherData.getTemperature().getTemp() - 273.15) + " C");
+                mTvHum.setText("" + weatherData.getCurrentCondition().getHumidity() + "%");
+                mTvPress.setText("" + weatherData.getCurrentCondition().getPressure() + " hPa");
+                mTvMaxTemp.setText("" + Math.round(weatherData.getTemperature().getMaxTemp() - 273.15) + " C");
+                mTvMinTemp.setText("" + Math.round(weatherData.getTemperature().getMinTemp() - 273.15) + " C");
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
-        // TODO: Use the ViewModel
-    }
-    private void loadWeatherData(String location){
-        mFetchWeatherTask.execute(this,location);
-    }
-    private static class FetchWeatherTask{
-        WeakReference<WeatherFragment> weatherFragmentWeakReference;
-        private ExecutorService executorService = Executors.newSingleThreadExecutor();
-        private Handler mainThreadHandler = HandlerCompat.createAsync(Looper.getMainLooper());
-
-        public void setWeakReference(WeatherFragment ref)
-        {
-            weatherFragmentWeakReference = new WeakReference<WeatherFragment>(ref);
-        }
-        public void execute(WeatherFragment ref, String location){
-
-            executorService.execute(new Runnable(){
-                @Override
-                public void run(){
-                    String jsonWeatherData;
-                    URL weatherDataURL = NetworkUtils.buildURLFromString(location);
-                    jsonWeatherData = null;
-                    try{
-                        jsonWeatherData = NetworkUtils.getDataFromURL(weatherDataURL);
-                        postToMainThread(jsonWeatherData);
-                    }catch(Exception e){
-                        e.printStackTrace();
-                    }
+                try {
+                    new NetworkUtils.DownloadImageTask((ImageView) mIvIcon)
+                            .execute(weatherData.getCurrentCondition().getIconCode());
+                    // Save Bitmap to WeatherData class
+                    weatherData.getCurrentCondition().setIcon(mIvIcon.getDrawingCache());
                 }
-            });
-        }
-
-        private void postToMainThread(String jsonWeatherData)
-        {
-            WeatherFragment localRef = weatherFragmentWeakReference.get();
-            mainThreadHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    if (jsonWeatherData != null) {
-                        try {
-                            localRef.mWeatherData = JSONWeatherUtils.getWeatherData(jsonWeatherData);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        if (localRef.mWeatherData != null) {
-                            localRef.mTvTemp.setText("" + Math.round(localRef.mWeatherData.getTemperature().getTemp() - 273.15) + " C");
-                            localRef.mTvHum.setText("" + localRef.mWeatherData.getCurrentCondition().getHumidity() + "%");
-                            localRef.mTvPress.setText("" + localRef.mWeatherData.getCurrentCondition().getPressure() + " hPa");
-                            localRef.mTvMaxTemp.setText("" + Math.round(localRef.mWeatherData.getTemperature().getMaxTemp() - 273.15) + " C");
-                            localRef.mTvMinTemp.setText("" + Math.round(localRef.mWeatherData.getTemperature().getMinTemp() - 273.15) + " C");
-
-                            try {
-                                new NetworkUtils.DownloadImageTask((ImageView) localRef.mIvIcon)
-                                        .execute(localRef.mWeatherData.getCurrentCondition().getIconCode());
-                                // Save Bitmap to WeatherData class
-                                localRef.mWeatherData.getCurrentCondition().setIcon(localRef.mIvIcon.getDrawingCache());
-                            }
-                            // If getting image from web fails, set image using a vector
-                            catch (Exception e){
-                                SetIcon(localRef.mIvIcon, localRef.mWeatherData.getCurrentCondition().getCondition());
-                                e.printStackTrace();
-                            }
-                        }
-                    }
+                // If getting image from web fails, set image using a vector
+                catch (Exception e){
+                    SetIcon(mIvIcon, weatherData.getCurrentCondition().getCondition());
+                    e.printStackTrace();
                 }
-            });
+            }
         }
+    };
+
+    // Pass location to view model
+    void loadWeatherData (String location) {
+        mViewModel.setLocation(location);
     }
 
     // Sets the weather imageview icon to one of five basic vectors
--- app/src/main/java/com/example/myapplication/ui/weather/WeatherViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/weather/WeatherViewModel.java
@@ -1,7 +1,26 @@
 package com.example.myapplication.ui.weather;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class WeatherViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.myapplication.weatherbackend.WeatherData;
+
+public class WeatherViewModel extends AndroidViewModel {
+    private MutableLiveData<WeatherData> jsonData;
+    private WeatherRepository mWeatherRepository;
+
+    public WeatherViewModel(Application application) {
+        super(application);
+        mWeatherRepository = WeatherRepository.getInstance(application);
+        jsonData = mWeatherRepository.getData();
+    }
+
+    public void setLocation(String location) {
+        mWeatherRepository.setLocation(location);
+    }
+
+    public  LiveData<WeatherData> getData() { return jsonData; }
 }
